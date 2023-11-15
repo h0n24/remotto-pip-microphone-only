6ec2f1f60a48180475f993473321b16073296e67
Jan Šablatura <h0n24.cz@gmail.com>
2023-11-15 04:05:24 +0100

    better comments

--- extension/script.js
+++ extension/script.js
@@ -2,7 +2,7 @@ function toggleMicrophone() {
   // get the current state of the microphone
   const currentState = APP.conference.isLocalAudioMuted();
 
-  // toggle the state of the microphone in the remotto
+  // toggle the state of the microphone in the Remotto
   APP.conference.toggleAudioMuted(!currentState);
 
   // toggle the state inside Picture-in-Picture window
@@ -10,24 +10,25 @@ function toggleMicrophone() {
 }
 
 function getRemottoMicrophoneState() {
-  // get state of the microphone in the remotto
+  // get state of the microphone in the Remotto
   const currentState = APP.conference.isLocalAudioMuted();
 
   // set the initial state of the microphone
-  // has to be opposite due to the isLocalAudioMuted() function
+  // has to be opposite due to the isLocalAudioMuted() function in Remotto
   navigator.mediaSession.setMicrophoneActive(!currentState);
 }
 
 function createPictureInPicture() {
-  // Add a button to the picture-in-picture mode via MediaSession's setActionHandler() method
+  // check if the browser supports the Picture-in-Picture API
   if ("mediaSession" in navigator) {
     getRemottoMicrophoneState();
 
+    // Add a button to the picture-in-picture mode via MediaSession's setActionHandler() method
     navigator.mediaSession.setActionHandler("togglemicrophone", () => {
       toggleMicrophone();
     });
 
-    // interval prevents the bug where user clicks on mute inside remotto,
+    // interval prevents the bug where user clicks on mute inside Remotto,
     // but the PIP is still on -> now it propagates the state of the microphone
     setInterval(() => {
       getRemottoMicrophoneState();
